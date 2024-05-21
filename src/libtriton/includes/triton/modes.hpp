97bd65cac672c1596402c636e836e345021ccf68
Elias Bachaalany <elias.bachaalany@gmail.com>
2024-05-20 17:32:13 -0700

    Update modes.hpp

--- src/libtriton/includes/triton/modes.hpp
+++ src/libtriton/includes/triton/modes.hpp
@@ -5,8 +5,7 @@
 **  This program is under the terms of the Apache License 2.0.
 */
 
-#ifndef TRITON_MODES_H
-#define TRITON_MODES_H
+#pragma once
 
 #include <memory>
 #include <unordered_set>
@@ -14,60 +13,38 @@
 #include <triton/dllexport.hpp>
 #include <triton/modesEnums.hpp>
 
+// The Modes namespace
+namespace triton::modes 
+{
+  // The modes class
+  class Modes 
+  {
+    private:
+      //! Copies a Modes.
+      void copy(const Modes& other);
 
+    protected:
+      //! The set of enabled modes
+      std::unordered_set<triton::modes::mode_e> enabledModes;
 
-//! The Triton namespace
-namespace triton {
-/*!
- *  \addtogroup triton
- *  @{
- */
+    public:
+      // Constructor
+      TRITON_EXPORT Modes();
+      TRITON_EXPORT Modes(const Modes& other);
 
-  //! The Modes namespace
-  namespace modes {
-  /*!
-   *  \ingroup triton
-   *  \addtogroup modes
-   *  @{
-   */
+      // Copies a Modes.
+      TRITON_EXPORT Modes& operator=(const Modes& other);
 
-    //! \class Modes
-    /*! \brief The modes class */
-    class Modes {
-      private:
-        //! Copies a Modes.
-        void copy(const Modes& other);
+      // Returns true if the mode is enabled.
+      TRITON_EXPORT bool isModeEnabled(triton::modes::mode_e mode) const;
 
-      protected:
-        //! The set of enabled modes
-        std::unordered_set<triton::modes::mode_e> enabledModes;
+      // Enables or disables a specific mode.
+      TRITON_EXPORT void setMode(triton::modes::mode_e mode, bool flag);
 
-      public:
-        //! Constructor.
-        TRITON_EXPORT Modes();
-
-        //! Constructor.
-        TRITON_EXPORT Modes(const Modes& other);
-
-        //! Copies a Modes.
-        TRITON_EXPORT Modes& operator=(const Modes& other);
-
-        //! Returns true if the mode is enabled.
-        TRITON_EXPORT bool isModeEnabled(triton::modes::mode_e mode) const;
-
-        //! Enables or disables a specific mode.
-        TRITON_EXPORT void setMode(triton::modes::mode_e mode, bool flag);
-
-        //! Clears recorded modes.
-        TRITON_EXPORT void clearModes(void);
-    };
-
-    //! Shared Modes.
-    using SharedModes = std::shared_ptr<triton::modes::Modes>;
-
-  /*! @} End of modes namespace */
+      // Clears recorded modes.
+      TRITON_EXPORT void clearModes(void);
   };
-/*! @} End of triton namespace */
-};
 
-#endif /* TRITON_MODES_H */
+  // Shared Modes.
+  using SharedModes = std::shared_ptr<triton::modes::Modes>;
+};
